3b51765bb5b31e7690bef0f5eb313ef901a6bf7f
Adem Kouki <ademkingnew@gmail.com>
2020-11-29 22:08:52 +0100

    Version 1.0.3

--- README.md
+++ README.md
@@ -1,20 +1,20 @@
 # Facebook Profile Picture Viewer
 
-![](https://i.imgur.com/oysTQEP.png)
+![](https://i.imgur.com/SKdCUeN.png)
 
 Bypass the Facebook profile picture guard and see user's profile picture in __full size__ 📷
 
-### How to Install
-[Click Here to Download from Chrome Web Store](https://chrome.google.com/webstore/detail/facebook-profile-picture/olhdholihhioakdjhfhligfehfjjfeoc?hl=fr&authuser=0)
+## [Click Here to Download](https://chrome.google.com/webstore/detail/facebook-profile-picture/olhdholihhioakdjhfhligfehfjjfeoc?hl=fr&authuser=0)
 
 
-### How to Install (From Github):
+### Installation Instructions:
 
-0. Download this repository
-1. Navigate to **chrome://extensions/** 
-2. Ensure the checkbox labeled **Developer mode** is enabled. 
-3. Click the button labeled **Load unpacked extension...**
-4. Select the directory 
+0. Download this repository as a [ZIP file from GitHub](https://github.com/Ademking/fb-profile-picture-viewer/archive/master.zip)
+1. Unzip the file and you should have a folder named fb-profile-picture-viewer-master.
+2. Navigate to **chrome://extensions/** 
+3. Ensure the checkbox labeled **Developer mode** is enabled. 
+4. Click the button labeled **Load unpacked extension**
+5. Select "fb-profile-picture-viewer-master" directory.
 
 ### How To Use
 
@@ -24,3 +24,25 @@ Bypass the Facebook profile picture guard and see user's profile picture in __fu
 ### Note
 
 I highly recommend to not use these kinds of tricks to find and view private profile pictures, because all people like to maintain some privacy and they don't want others to disclose their privacy.
+
+Please take in consideration that Facebook is updating its Website regularly and thus could make the code throw errors. If any error encountered please open an new issue request
+
+### Changelog
+
+__1.0.3__
+- Fixed an issue with logged user profile
+
+__1.0.2__
+- Fixed "Access token" problem. Everything is working again
+
+__1.0.1__
+- Fixed silhouette image bug
+
+### Author
+
+[Adem Kouki](https://github.com/Ademking)
+
+### Contributing
+
+Open an issue or a pull request to suggest changes or additions
+
--- contextmenu.js
+++ contextmenu.js
@@ -1,5 +1,5 @@
 /**
- * Facebook Profile Picture Viewer v1.0.1
+ * Facebook Profile Picture Viewer
  * -------------------
  * See Facebook user's profile picture in full size
  * -------------------
@@ -8,21 +8,23 @@
  * Github: https://github.com/Ademking/fb-profile-picture-viewer
  */
 
-
 /**
  * Returns Current Tab URL
  */
 function get_current_tab_url() {
-    return new Promise((resolve, reject) => {
-        chrome.tabs.query({
-            active: true,
-            lastFocusedWindow: true
-        }, tabs => {
-            var tab = tabs[0];
-            var tab_url = tab.url;
-            resolve(tab_url);
-        });
-    });
+  return new Promise((resolve, reject) => {
+    chrome.tabs.query(
+      {
+        active: true,
+        lastFocusedWindow: true,
+      },
+      (tabs) => {
+        var tab = tabs[0];
+        var tab_url = tab.url;
+        resolve(tab_url);
+      }
+    );
+  });
 }
 
 /**
@@ -30,14 +32,14 @@ function get_current_tab_url() {
  * @param link Facebook Profile URL
  */
 function get_current_username(link) {
-    return new Promise((resolve, reject) => {
-        const test = new URL(link);
-        if (test.pathname === "/profile.php") {
-            resolve(test.searchParams.get("id"));
-        } else {
-            resolve(test.pathname);
-        }
-    })
+  return new Promise((resolve, reject) => {
+    const test = new URL(link);
+    if (test.pathname === "/profile.php") {
+      resolve(test.searchParams.get("id"));
+    } else {
+      resolve(test.pathname);
+    }
+  });
 }
 
 /**
@@ -45,87 +47,67 @@ function get_current_username(link) {
  * @param username Username
  */
 function get_username_id(username) {
-    return new Promise((resolve, reject) => {
-        fetch('https://mbasic.facebook.com/' + username).then(response => {
-            return response.text();
-        }).then(html => {
-            const regex = /owner_id=([a-z0-9\-]+)\&?/i;
-            var regex_res = html.match(regex);
-            if (regex_res) {
-                resolve(regex_res[1])
-            } else {
-                alert("Could not extract Facebook Profile Picture")
-                reject(new Error(`Could not extract Facebook Profile Picture`));
-            }
-        }).catch(err => {
-            alert("Could not extract Facebook Profile Picture")
-            reject(new Error(`Could not extract Facebook Profile Picture`));
-        });
-    })
+  return new Promise((resolve, reject) => {
+    fetch("https://mbasic.facebook.com/" + username)
+      .then((response) => {
+        return response.text();
+      })
+      .then((html) => {
+        const regex = /owner_id=([a-z0-9\-]+)\&?/i;
+        var regex_res = html.match(regex);
+        if (regex_res) {
+          resolve(regex_res[1]);
+        } else {
+          alert("Could not extract Facebook Profile Picture");
+          reject(new Error(`Could not extract Facebook Profile Picture`));
+        }
+      })
+      .catch((err) => {
+        alert("Could not extract Facebook Profile Picture");
+        reject(new Error(`Could not extract Facebook Profile Picture`));
+      });
+  });
 }
 
 /**
  * Get User Access Token
  */
 function get_fb_access_token() {
-    // return new Promise((resolve, reject) => {
-        // fetch("https://m.facebook.com/composer/ocelot/async_loader/?publisher=feed")
-            // .then(response => {
-                // return response.text();
-            // })
-            // .then(code => {
-                // const regex = /accessToken\\":\\"(.*?)\\"/;
-                // var regex_res = code.match(regex);
-                // if (regex_res) {
-                    // resolve(regex_res[1])
-                // } else {
-                    // alert("Could not get your facebook access token. Please check if you're logged in")
-                    // reject(new Error(`Could not get your facebook access token. Please check if you're logged in`));
-                // }
-            // })
-            // .catch(err => {
-                // alert("Could not get your facebook access token. Please check if you're logged in")
-                // reject(new Error(`Could not get your facebook access token. Please check if you're logged in`));
-            // });
-    // })
-	
-	// Update version 1.0.2: Fixed Access token	
-	return new Promise((resolve, reject) => {
-		resolve("6628568379%7Cc1e620fa708a1d5696fb991c1bde5662")
-	});
+  // Using fb android client token
+  return new Promise((resolve, reject) => {
+    resolve("6628568379%7Cc1e620fa708a1d5696fb991c1bde5662");
+  });
 }
-
-
 
 /**
  * Opens Full Size Profile Picture using fb access token
  * @param id FB Profile ID
  */
 function open_full_hd_photo(id) {
-
-    get_fb_access_token() // Get user access token
-    .then(access_token => {
-        window.open(`https://graph.facebook.com/${id}/picture?width=5000&access_token=${access_token}`);
-    })
-
+  get_fb_access_token() // Get user access token
+    .then((access_token) => {
+      window.open(
+        `https://graph.facebook.com/${id}/picture?width=5000&access_token=${access_token}`
+      );
+    });
 }
 
 /**
  * When extension started from Context menu
  */
 function start() {
-    get_current_tab_url()
-        .then(get_current_username)
-        .then(get_username_id)
-        .then(open_full_hd_photo);
+  get_current_tab_url()
+    .then(get_current_username)
+    .then(get_username_id)
+    .then(open_full_hd_photo);
 }
 
 /**
  * Creates Context Menu
  */
 chrome.contextMenus.create({
-    title: 'Unlock full size profile picture',
-    contexts: ['all'],
-    onclick: start,
-    documentUrlPatterns: ["*://*.facebook.com/*"]
+  title: "Unlock full size profile picture",
+  contexts: ["all"],
+  onclick: start,
+  documentUrlPatterns: ["*://*.facebook.com/*"],
 });
